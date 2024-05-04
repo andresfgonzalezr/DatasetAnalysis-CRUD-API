75265e53b3f98c46ddfa2f429333f748f0ea2cf0
Andres Gonzalez <andresgonzalez@MacBook-Air-de-Andres.local>
2024-05-04 01:41:36 -0500

    deleting CRUD Class

--- database/crud.py
+++ database/crud.py
@@ -1,44 +1,41 @@
 from sqlalchemy.sql.expression import text
 from database.models import Datos
-from database.database import Session, session
+from database.database import Session, session, engine
 import json
 from database.database import client
 
 
-class CRUD:
-    # The function has the parameter data, the new data comes from the class Datos, creating the new object, and then add to the db and to confirm it make a commit
-    @staticmethod
-    def create(data, db):
-        new_data = Datos(**data)
-        db.add(new_data)
-        db.commit()
+def create(data, db):
+    new_data = Datos(**data)
+    db.add(new_data)
+    db.commit()
 
-    @staticmethod
-    # Making a request to show all the table
-    def read(db):
-        query = text("SELECT * FROM final_data_andres")
-        return db.execute(query).fetchall()
 
-    @staticmethod
-    def read_by_id(data_id, db):
-        query = text("SELECT * FROM final_data_andres WHERE id= :data_id")
-        result = db.execute(query, {"data_id": data_id}).fetchall()
-        return result
+# Making a request to show all the table
+def read(db):
+    query = text("SELECT * FROM final_data_andres")
+    return db.execute(query).fetchall()
 
-    @staticmethod
-    # Making an update from one id in the DataBase, the value data_id is the same id from the table, and the new data is the values that are going to be updated
-    def update_data(data_id, new_data, db):
-        data_to_update = db.query(Datos).filter_by(id=data_id).first()
-        for key, value in new_data.items():
-            setattr(data_to_update, key, value)
-        db.commit()
 
-    @staticmethod
-    # Deleting one row from the DataBase with the id
-    def delete_data(data_id, db):
-        data_to_delete = session.query(Datos).filter_by(id=data_id).first()
-        db.delete(data_to_delete)
-        db.commit()
+def read_by_id(data_id, db: session):
+    query = text("SELECT * FROM final_data_andres WHERE id= :data_id")
+    result = db.execute(query, {"data_id": data_id}).fetchall()
+    return result
+
+
+# Making an update from one id in the DataBase, the value data_id is the same id from the table, and the new data is the values that are going to be updated
+def update_data(data_id, new_data, db):
+    data_to_update = db.query(Datos).filter_by(id=data_id).first()
+    for key, value in new_data.items():
+        setattr(data_to_update, key, value)
+    db.commit()
+
+
+# Deleting one row from the DataBase with the id
+def delete_data(data_id, db):
+    data_to_delete = session.query(Datos).filter_by(id=data_id).first()
+    db.delete(data_to_delete)
+    db.commit()
 
 
 def get_gpt(input_prompt):
@@ -84,12 +81,12 @@ def get_gpt(input_prompt):
     data_id_gpt = data_dictionary["id"]
 
     if data_response["action"] == "create":
-        return CRUD.create(data_dictionary, db)
+        return create(data_dictionary, db)
     elif data_response["action"] == "read":
-        return CRUD.read_by_id(data_id_gpt, db)
+        return read_by_id(data_id_gpt, db)
     elif data_response["action"] == "update":
-        return CRUD.update_data(data_id_gpt, data_dictionary, db)
+        return update_data(data_id_gpt, data_dictionary, db)
     elif data_response["action"] == "delete":
-        return CRUD.delete_data(data_id_gpt, db)
+        return delete_data(data_id_gpt, db)
 
-print(get_gpt("read dataframe with id 1"))
+# print(get_gpt("read dataframe with id 1"))
